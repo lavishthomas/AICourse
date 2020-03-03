cb6151fcc48b0d09a360eb8263ffe33bc299c6f7
Lavish Thomas <L00150445@student.lyit.ie>
2020-03-03 12:05:40 +0000

    First class

--- commands.sh
+++ commands.sh
@@ -2,7 +2,12 @@ Conda env create -f nlp_environment.yml
 conda activate AI2_course
 conda deactivate
 
+cd C:\Users\L00150445\.conda\envs\AI2_course>
+pip install 
+pip install jupyterthemes
+jt -t chesterish
+
 git pull origin master
 git add -A
-git commit -m "Updated ReadMe"
+git commit -m "First class "
 git push origin master
